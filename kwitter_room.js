389a406b9e60a76316ef6300bf83922075c79a96
JazRiv <110856832+JazRiv@users.noreply.github.com>
2023-02-10 17:40:14 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD8qVnliXMwFKl1izQQM2hO9IRUcokRjdA",
+  authDomain: "chat-talk-1b82c.firebaseapp.com",
+  databaseURL: "https://chat-talk-1b82c-default-rtdb.firebaseio.com",
+  projectId: "chat-talk-1b82c",
+  storageBucket: "chat-talk-1b82c.appspot.com",
+  messagingSenderId: "922462400746",
+  appId: "1:922462400746:web:d2136dac09a8c6cfd22793"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
